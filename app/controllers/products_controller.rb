5d637066a8959c06ef64a4173487c6f6cbea0aca
Iyad Horani <i3d@ironic3d.com>
2014-08-25 23:12:41 +1000

    Removed :user_id from product_param to set current_user explicitly

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :permalink, :description, :price, :user_id, :file)
+      params.require(:product).permit(:name, :permalink, :description, :price, :file)
     end
 end
