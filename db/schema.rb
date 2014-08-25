b0fff39109b36035d49c0f7ef9ebaee1a292c68c
Iyad Horani <i3d@ironic3d.com>
2014-08-25 23:12:57 +1000

    Add a Sale scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140825130110) do
+ActiveRecord::Schema.define(version: 20140825131231) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -21,9 +21,23 @@ ActiveRecord::Schema.define(version: 20140825130110) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
   end
 
   add_index "products", ["user_id"], name: "index_products_on_user_id"
+
+  create_table "sales", force: true do |t|
+    t.string   "email"
+    t.string   "guid"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sales", ["product_id"], name: "index_sales_on_product_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
