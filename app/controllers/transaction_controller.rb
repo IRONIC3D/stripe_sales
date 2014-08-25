a518bd93255fcf6c57f338f388916d15b8a3240f
Iyad Horani <i3d@ironic3d.com>
2014-08-26 07:44:34 +1000

    Add rescue for transaction_controller

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -16,9 +16,27 @@ class TransactionController < ApplicationController
     token = params[:stripeToken]
 
     begin
-      
-    rescue Exception => e
-      
+      charge = Stripe::Charge.create( 
+        amount: product.price,
+        currency: "usd",
+        card: token,
+        description: params[:email]
+      )
+      redirect_to_pickup_url(guid: @sale.guid)
+    rescue Stripe::CardError => e
+      # The card has been declined or
+      # some other error has occured
+      @error = e
+      render :new
     end
+  end
+
+  def download
+    @sale = Sale.find_by!(guid: params[:guid])
+    resp = HTTParty.get(@sale.product.file.url)
+    filename = @sale.product.file.url
+    send_data resp.body,
+      :filename => File.basename(filename),
+      :content_type => resp.headers['Content-Type']
   end
 end
