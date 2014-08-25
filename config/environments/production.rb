8f49798450af1c13f3d140f123ef823bafdc0690
Iyad Horani <i3d@ironic3d.com>
2014-08-25 23:33:05 +1000

    Add S3 configs to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,11 +91,21 @@ Rails.application.configure do
   authentication: :plain
   'smtp.mandrillapp.com',
   587,
-  ENV['MANDRILL_USERNAMheroku appE'],
+  ENV['MANDRILL_USERNAME'],
   ENV['MANDRILL_APIKEY'],
   'heroku.com',
   }
   config.action_mailer.default_url_options = {
     :host => 'sell-me-something.herokuapp.com'
   }
+
+  # S3 env setup
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    } 
+}
 end
