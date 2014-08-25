a28377f83417feafae403f3575c82070a4d918c7
Iyad Horani <i3d@ironic3d.com>
2014-08-26 07:41:16 +1000

    Update routes manually

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   resources :products
 
   devise_for :users
+
+  get   '/buy/:permalink',  to: 'transactions#new',       as: :show_buy
+  post  '/buy/:permalink',  to: 'transactions#create',    as: :buy
+  get   '/pickup/:guid',    to: 'transactions#pickup',    as: :pickup
+  get   '/download/:guid'   to: 'transactions#download',  as: :download
+
   root to: 'products#index'
 end
